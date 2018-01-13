99b7a32dd3e10c1062f579e3c285dd0fe5513a89
waverider <waverider04@gmail.com>
2018-01-12 17:42:34 -0700

    Implements MVP version of FlaskTaskr (minus user management).

--- project/views.py
+++ project/views.py
@@ -6,7 +6,11 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, \
-request, session, url_for
+request, session, url_for, g
+
+from forms import AddTaskForm
+
+import pdb
 
 
 
@@ -50,7 +54,8 @@ def logout():
 @app.route('/', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+		if request.form['username'] != app.config['USERNAME'] \
+		or request.form['password'] != app.config['PASSWORD']:
 			error = 'Invalid credentials. Please try again.'
 			return render_template('login.html', error=error)
 		else:
@@ -58,5 +63,92 @@ def login():
 			flash('Welcome!')
 			return redirect(url_for('tasks'))
 	return render_template('login.html')
+
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+	g.db = connect_db()
+	cursor = g.db.execute(
+	 	'SELECT name, due_date, priority, task_id FROM tasks WHERE status=1'
+	)
+	open_tasks = [
+		dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cursor.fetchall()
+	]
+	cursor = g.db.execute(
+	 	'SELECT name, due_date, priority, task_id FROM tasks WHERE status=0'
+	)
+	closed_tasks = [
+		dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cursor.fetchall()
+	]
+	g.db.close()
+	return render_template(
+		'tasks.html',
+		form=AddTaskForm(request.form),
+		open_tasks=open_tasks,
+		closed_tasks=closed_tasks
+	)
+
+
+
+# Add new tasks
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+	g.db = connect_db()
+	name = request.form['name']
+	date = request.form['due_date']
+	priority = request.form['priority']
+	if not name or not date or not priority:
+		g.db.close() # NOT IN EXAMPLE CODE!
+		flash("All fields are required. Please try again.")
+		return redirect(url_for('tasks'))
+	else:
+		g.db.execute('INSERT INTO tasks (name, due_date, priority, status) \
+			VALUES (?, ?, ?, 1)', [
+				request.form['name'],
+				request.form['due_date'],
+				request.form['priority']
+			]
+		)
+		g.db.commit()
+		g.db.close()
+		flash('New entry was successfully posted. Thanks.')
+		return redirect(url_for('tasks'))
+
+
+
+
+# Mark tasks as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+	g.db = connect_db()
+	g.db.execute(
+		'UPDATE tasks SET status=0 WHERE task_id=' + str(task_id)
+	)
+	g.db.commit()
+	g.db.close()
+	flash('The task was marked as complete.')
+	pdb.set_trace() ### REMOVE ###
+	return redirect(url_for('tasks'))
+
+
+
+
+# Delete tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+	g.db = connect_db()
+	g.db.execute('DELETE FROM tasks WHERE task_id=' + str(task_id))
+	g.db.commit()
+	g.db.close()
+	flash('The task was deleted.')
+	return redirect(url_for('tasks'))
+
+
+
 
 
