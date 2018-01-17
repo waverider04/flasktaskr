61478686cd902d94c54357b15a5a82032ee7fe81
waverider <waverider04@gmail.com>
2018-01-16 22:16:54 -0700

    Refactors FlaskTaskr application so that it conforms to the Blueprints design pattern.

---
+++ db_create.py
@@ -0,0 +1,21 @@
+# db_crreate.py
+
+from project import db
+from project.models import Task, User
+from datetime import date
+
+
+
+# create the database and the db tables
+db.create_all()
+
+# insert data
+db.session.add(
+	Task("Finish this tutorial", date(2018, 2, 1), 10, date(2018, 2, 1), 1, 1)
+)
+db.session.add(
+	Task("Finish Real Python", date(2018, 2, 1), 10, date(2018, 2, 1), 1, 1)
+)
+
+# commit the changes
+db.session.commit()
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,12 @@
-# project/test_users.py
+# tests/test_users.py
 
 
 import os
 import unittest
 
-from views import app, db
-from _config import basedir
-from models import User
+from project import app, db
+from project._config import basedir
+from project.models import Task, User
 
 TEST_DB = 'test.db'
 
