2e7a1771f70b19c356f204730c6d6c9ca28d6245
waverider <waverider04@gmail.com>
2018-01-23 06:57:21 -0700

    Adds error logging for 404 and 500 errors, and makes minor refinements to automated testing (all 33 tests currently passing).

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
 # project/__init__.py
 
 
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -23,10 +24,23 @@ app.register_blueprint(tasks_blueprint)
 
 # register error handlers
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(error):
+	if app.debug is not True:
+		now = datetime.datetime.now()
+		r = request.url
+		with open('error.log', 'a') as f:
+			current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+			f.write("\n404 error at {}: {}".format(current_timestamp, r))
 	return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
+	db.session.rollback()
+	if app.debug is not True:
+		now = datetime.datetime.now()
+		r = request.url
+		with open('error.log', 'a') as f:
+			current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+			f.write("\n500 error at {}: {}".format(current_timestamp, r))
 	return render_template('500.html'), 500
--- project/test_main.py
+++ project/test_main.py
@@ -4,7 +4,6 @@
 import os
 import unittest
 
-
 from project import app, db
 from project._config import basedir
 from project.models import User
@@ -12,70 +11,63 @@ from project.models import User
 
 TEST_DB = 'test.db'
 
+
 class MainTests(unittest.TestCase):
 
+    ############################
+    #### setup and teardown ####
+    ############################
 
-	###########################
-	#### setup an teardown ####
-	###########################
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
 
+        self.assertEquals(app.debug, False)
 
-	# executed prior to each test
-	def setUp(self):
-		app.config['TESTING'] = True
-		app.config['WTF_CSRF_ENABLED'] = False
-		app.config['DEBUG'] = False
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-			os.path.join(basedir, TEST_DB)
-		self.app = app.test_client()
-		db.create_all()
-
-		self.assertEquals(app.debug, False)
-
-
-	# executed after each test
-	def tearDown(self):
-		db.session.remove()
-		db.drop_all()
+    # executed after each test
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
 
-	########################
-	#### helper methods ####
-	########################
+    ########################
+    #### helper methods ####
+    ########################
+
+    def login(self, name, password):
+        return self.app.post('/', data=dict(
+            name=name, password=password), follow_redirects=True)
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_404_error(self):
+        response = self.app.get('/this-route-does-not-exist/')
+        self.assertEquals(response.status_code, 404)
+        self.assertIn(b'Sorry. There\'s nothing here.', response.data)
+
+    def test_500_error(self):
+        bad_user = User(
+            name='Jeremy',
+            email='jeremy@realpython.com',
+            password='django'
+        )
+        db.session.add(bad_user)
+        db.session.commit()
+        self.assertRaises(ValueError, self.login, 'Jeremy', 'django')
+        try:
+            response = self.login('Jeremy', 'django')
+            self.assertEquals(response.status_code, 500)
+        except ValueError:
+            pass
 
 
-	def login(self, name, password):
-		return self.app.post('/', data=dict(
-			name=name, password=password), follow_redirects=True)
-
-
-	###############
-	#### tests ####
-	###############
-
-
-	def test_404_error(self):
-		response = self.app.get('/this-route-does-not-exist/')
-		self.assertEqual(response.status_code, 404)
-		self.assertIn(b"Sorry. There's nothing here.", response.data)
-
-
-	def test_500_error(self):
-		bad_user = User(
-			name='JohnDoe',
-			email='JohnDoe@pyFreelance.com',
-			password='JohnnyLikesFlask'
-		)
-		db.session.add(bad_user)
-		db.session.commit()
-		self.assertRaises(ValueError, self.login, 'JohnDoe', 'JohnnyLikesFlask')
-		response = self.login('JohnDoe', 'JohnnyLikesFlask')
-		self.assertEqual(response.status_code, 500)
-		# try:
-		#  	response = self.login('JohnDoe', 'JohnnyLikesFlask')
-		#  	self.assertEqual(response.status_code, 500)
-		# except ValueError:
-		# 	pass
-
-if __name__ == '__main__':
-	unittest.main()
+if __name__ == "__main__":
+    unittest.main()
